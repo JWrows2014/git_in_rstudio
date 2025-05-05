eec11deea22401753bffc6a155ecddb00b24fa0b
joe.wu <wuz8@nih.gov>
2025-05-05 16:33:39 -0400

    Added box plot to show the distribution of mpg for each cylinder category.

--- git_in_rstudio.R
+++ git_in_rstudio.R
@@ -3,10 +3,11 @@
 # Load data
 data(mtcars)
 
-
-
 # Load tidyverse
 library(tidyverse)
 
 # Get average mpg by for each cylinder
 mtcars %>% group_by(cyl) %>% summarise(mean_mpg=mean(mpg))
+
+# Look at distribution of mpg by cylinder using box plot
+ggplot(mtcars, aes(x=as.factor(cyl), y=mpg))+geom_boxplot()+xlab("number of cylinders")
