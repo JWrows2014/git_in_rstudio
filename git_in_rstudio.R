d7f54e776b8e26fa1524263f5ec4b1dcf61a080a
joe.wu <wuz8@nih.gov>
2025-05-05 15:06:43 -0400

    Deleted the line View(mtcars), loaded tidyverse, and calculated the average mpg for each cylinder class car.

--- git_in_rstudio.R
+++ git_in_rstudio.R
@@ -3,5 +3,10 @@
 # Load data
 data(mtcars)
 
-# Take a look at mtcars
-View(mtcars)
+
+
+# Load tidyverse
+library(tidyverse)
+
+# Get average mpg by for each cylinder
+mtcars %>% group_by(cyl) %>% summarise(mean_mpg=mean(mpg))
