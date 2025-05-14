2bc9f6b8042d20ac2a008c6f9779f0bd2d7f7b38
joe.wu <wuz8@nih.gov>
2025-05-14 10:27:45 -0400

    Plotted correlation between hp and mpg for mtcars.

--- git_in_rstudio.R
+++ git_in_rstudio.R
@@ -12,5 +12,8 @@ mtcars %>% group_by(cyl) %>% summarise(mean_mpg=mean(mpg))
 # Look at distribution of mpg by cylinder using box plot
 ggplot(mtcars, aes(x=as.factor(cyl), y=mpg))+geom_boxplot()+xlab("number of cylinders")
 
-# Correlation between displacement and quarter mile time
+# Correlation between hp and quarter mile time
 ggplot(mtcars, aes(x=hp,y=qsec))+geom_point()
+
+# Correlation between hp and mpg
+ggplot(mtcars, aes(x=hp,y=mpg))+geom_point()
