f221fa39343113319c0a7a0a06b7bd2c63d4db0a
Joe Wu <wuz8@nih.gov>
2025-05-19 14:17:08 -0400

    Started development1 branch and calculated quarter mile time by cylinder for mtcars.

--- git_in_rstudio.R
+++ git_in_rstudio.R
@@ -11,3 +11,6 @@ mtcars %>% group_by(cyl) %>% summarise(mean_mpg=mean(mpg))
 
 # Correlation between displacement and quarter mile time
 ggplot(mtcars, aes(x=hp,y=qsec))+geom_point()
+
+# Get average quarter mile time by cylinder
+mtcars %>% group_by(cyl) %>% summarise(mean_qsec=mean(qsec))
